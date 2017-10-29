fa69ffe1ce83dafd233b213600d89ebacdc7aaf2
Lam Ho <hotienlam@gmail.com>
2017-10-29 22:24:01 +0700

    Section 5 - Project: Maths Game (HTML, CSS & JS): update javascript code

--- WEBSITES/2.MathsGame(JS)/javascript.js
+++ WEBSITES/2.MathsGame(JS)/javascript.js
@@ -1,16 +1,66 @@
-// if we click on the start/reset
-    // if we are playing
         // reload page
     // if we are not playing
-        // set score to 0
-        // show countdown box
         // reduce time by 1 sec in loop
             // timeleft?
                 // yes -> continue
                 // no -> game over
-        // change button to reset
         // generate new Q&A
 
+var playing = false;
+var score;
+var timeRemaining;
+// if we click on the start/reset
+document.getElementById("startreset").onclick = function() {
+    // if we are playing
+    if (playing) {
+        location.reload(); //reload page
+    } else {
+        playing = true;
+        hide("gameover");
+        // set score to 0
+        score = 0;
+        
+        // show countdown box
+        timeRemaining = 60;
+        show("timeremaining");
+        
+        // change button to reset
+        document.getElementById("startreset").innerHTML = "Reset Game";
+        
+        // show countdown box
+        startCountdown();
+    }
+}
+
+function startCountdown() {
+    action = setInterval(function() {
+        timeRemaining--;
+        document.getElementById("timeremainingvalue").innerHTML = timeRemaining;
+        if (timeRemaining == 0) {
+            // Game Over!
+            stopCountdown();
+            show("gameover");
+            document.getElementById("gameover").innerHTML = "<p>Game Over!</p><p>Your score is " + score + ".</p>";
+            document.getElementById("startreset").innerHTML = "Start Game";
+            playing = false;
+        }
+    }, 1000);
+}
+
+function stopCountdown() {
+    clearInterval(action);
+}
+
+function show(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+/*
+/////////////////////////////////////////////////////
+var score = 0;
 function startReset() {
     window.console.log("User click startreset");
     if (isPlaying()) {
@@ -27,7 +77,7 @@ function startReset() {
                 window.console.log("Time is remaining");
                 timeremaining--; document.getElementById("timeremainingvalue").innerHTML = timeremaining;
             } else {
-                document.getElementById("gameover").innerHTML = "<p>Game Over!</p><p>Your score is " + document.getElementById("scorevalue").innerHTML + "</p>";
+                document.getElementById("gameover").innerHTML = "<p>Game Over!</p><p>Your score is " + score + "</p>";
                 document.getElementById("gameover").style.display = "initial";
             }
         }, 1000);
@@ -50,7 +100,8 @@ function generateNewQA() {
     var y = Math.round(Math.random() * 10);
     var question = x + " x " + y;
     document.getElementById("question").innerHTML = question;
-    var correctBox = Math.round(Math.random() * 4);
+    var correctBox = Math.floor(Math.random() * 4) + 1;
+    window.console.log("Correct box: " + correctBox);
     var correctAnswer = x * y;
     for (i = 1; i < 5; i++) {
         if (i == correctBox) {
@@ -75,3 +126,30 @@ function generateNewQA() {
                 // generate new Q&A
             // no
                 // show try again box for 1 sec
+
+function checkAnswer(boxId) {
+    if (isPlaying()) {
+        if (isCorrectAnswer(boxId)) {
+//            var scoreValue = document.getElementById("scorevalue").innerHTML + 1;
+//            scoreValue += 1;
+            score++;
+            window.console.log("Score: " + score);
+            document.getElementById("scorevalue").innerHTML = score;
+            document.getElementById("correct").style.display = "initial";
+            setTimeout(function() {document.getElementById("correct").style.display = "none";}, 1000)
+            generateNewQA();
+        } else {
+            document.getElementById("wrong").style.display = "initial";
+            setTimeout(function() {document.getElementById("wrong").style.display = "none";}, 1000)
+        }
+    }
+}
+
+function isCorrectAnswer(boxId) {
+    var chosenAnswer = document.getElementById(boxId).innerHTML;
+    var question = document.getElementById("question").innerHTML;
+    var numbers = question.split(" x ");
+    var correctAnswer = numbers[0] * numbers[1];
+    return (chosenAnswer == correctAnswer);
+}
+*/
